06f84693cbdc1e82bc2053e9de0e537ff0f3fe1e
Takatoshi Ono <takatoshi.ono@pepabo.com>
2014-07-22 13:33:47 +0900

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718042440) do
+ActiveRecord::Schema.define(version: 20140722043252) do
+
+  create_table "activities", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "drinking"
+    t.string   "memo"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["user_id"], name: "index_activities_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "nickname",   null: false
